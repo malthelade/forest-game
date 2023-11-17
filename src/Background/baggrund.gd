458db6495ba8d093d4765ad820119349ea831402
Kristian <krisgamer@live.com>
2023-11-17 12:07:06 +0100

    background and house collision fix

--- src/Background/baggrund.gd
+++ src/Background/baggrund.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
 @onready var bulldozer = $Bulldozer
-@onready var hys = $Hys
+@onready var house = $House
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	bulldozer.move_target = hys.position
+	bulldozer.move_target = house.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
