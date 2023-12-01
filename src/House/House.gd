2b1f02d55422a8d1c21a3195dc387b40edd967fc
Kristian <krisgamer@live.com>
2023-12-01 10:14:42 +0100

    poop

--- src/House/House.gd
+++ src/House/House.gd
@@ -5,23 +5,16 @@ var Health = 250
 
 signal gameover
 
-signal gameover
-
-
 #When the timer stops the house will lose health
 func _on_timer_timeout():
-	Health -= 50
+	Health -= 10
 	if Health <= 0:
 		no_health.rpc()
-		
-		
+
 func _on_area_entered(area):
 	if area.is_in_group("bulldozer"):
 		timer.start()
-
-func _on_area_exited(area):
-	if area.is_in_group("bulldozer"):
-		timer.stop()
+		
 
 @rpc("any_peer","call_local")
 func no_health():
